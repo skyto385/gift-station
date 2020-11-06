01f005b8a1fe110514e92c46eb2ea67d0dce0da3
Makoto <74003141+skyto385@users.noreply.github.com>
2020-11-06 14:35:01 +0900

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(function(){
+    var swiper = new Swiper('.swiper-container', {
+        loop: true,
+        autoplay: {
+            delay: 6000,
+            speed: 600,
+        },
+    });
+});
